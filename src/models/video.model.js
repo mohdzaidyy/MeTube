a817fe2a4b4aa85c3869e37190c3027a347b79c5
Mohd Zaid <mohdzaid83484@gmail.com>
2026-06-29 23:50:36 +0530

    Add user and video models with JWT and bcrypt integration

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, {Schema} from 'mongoose';
+
+const videoSchema = new Schema(
+    {
+        videoFile : {
+            type : String,  //Cloudinary url
+            required : true,
+        },
+        thumbnail : {
+            type : String,  //Cloudinary url
+            required : true,
+        },
+        title : {
+            type : String,
+            required : true,
+        },
+        description : {
+            type : String,
+            required : true,
+        },
+        duration : {
+            type : Number,
+            default : 0
+        },
+        isPublished : {
+            type : Boolean,
+            default : true,
+        },
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        }
+    },
+    {
+        timestamps : true,
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePagination)
+
+export const Video = mongoose.model("Video", videoSchema)
